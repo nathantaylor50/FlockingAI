df7cfd669bf2b07c3ab5946e6b3e1e9c28d7ba15
nathantaylor50 <nathantaylor50@msn.com>
2016-05-18 19:45:47 +0100

    fov

--- Assets/AgentConfig.cs
+++ Assets/AgentConfig.cs
@@ -1,18 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AgentConfig : MonoBehaviour {
+public class AgentConfig : MonoBehaviour
+{
 
-	public float Rc;
-	public float Rs;
-	public float Ra;
+    public float Rc;        // cohesion radius
+    public float Rs;        // separation radius
+    public float Ra;        // alignment radius
+    public float Ravoid;    // avoidance radius
 
-	public float Kc;
-	public float Ks;
-	public float Ka;
 
-	public float MaxFieldOfViewAngle = 180;
+    public float Kc;        // cohesion coeficient
+    public float Ks;        // separation coeficient
+    public float Ka;        // alignment coeficient
+    public float Kw;        // wander coeficient
+    public float Kavoid;    // avoidance coeficient
 
-	public float maxA;
-	public float maxV;
+    public float MaxFieldOfViewAngle = 180;     // maximum field of view
+
+    public float WanderJitter;      // size of jitter for wander
+    public float WanderRadius;      // wander radius
+    public float WanderDistance;    // wander distance from center of agent
+
+    public float maxA;      // maximum acceleration
+    public float maxV;      // maximum velocity
 }
